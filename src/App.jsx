f5cc74237dfef8b23ffbf4ce905987dbd3eaeeef
Flavio <flavio.defreitas@ynov.com>
2022-09-12 19:10:43 +0200

    Resize Container Image

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import Tel from "./assets/telephone.png";
 import Git from "./assets/github.png";
-import TLS from "./assets/ProjetTLS.png";
-import Myrhmica from "./assets/Myrhmica.png";
 import info from "./assets/info.png";
 import projet from "./assets/projet.png";
 import ExpPro from "./assets/mallette.png";
@@ -11,7 +9,7 @@ import contact from "./assets/contact.png";
 
 function App() {
   return (
-    <div class="WrapperContent">
+    <div className="WrapperContent">
       <div class="Header" className="header">
         <a href="#About">À Propos</a>
         <a href="#Projets">Projets</a>
@@ -37,7 +35,7 @@ function App() {
         </div>
       </div>
 
-      <section id="#About" class="WrapperTop">
+      <div id="#About" className="WrapperTop">
         <h1 className="Title">De Freitas Flavio</h1>
         <h2 className="apropos">À propos</h2>
         <p className="Sfont p-4">
@@ -46,40 +44,46 @@ function App() {
           technologies, de science et d'espace, doté d’une grande curiosité et
           de motivation, en quête de nouveaux challenges
         </p>
-      </section>
-
-      <section id="Projets" className="MfontTitle">
-        Projets
-      </section>
-      <div class="WrapperProjets">
-        <div className="WrapperBox">
-          <img src={TLS} className="Image" />
-        </div>
-
-        <div class="WrapperInfos">
-          <p className="Strong">The Last Survivor on the space</p>
-          <p className="Sfont">Shoot'em up d'alien, top-down shooter blabla</p>
-        </div>
-        <button className="Plus">En savoir plus</button>
       </div>
 
-      <div
-        class="TextHeader"
-        id="ExperienceProfesionnel"
-        className="MfontTitle"
-      >
-        Experience Profesionnel
+      <div className="SubTitle">
+        <h2 id="Projets" className="MfontTitle">
+          Projets
+        </h2>
       </div>
-      <div class="WrapperExpPro">
-        <div className="WrapperBox">
-          <img src={Myrhmica} className="Image" />
-        </div>
+      <div className="w-full flex justify-center items-center">
+        <div className="WrapperBox Tls bg-no-repeat bg-contain bg-left mr-6"></div>
 
-        <div class="WrapperInfos">
+        <div className="w-1/3 flex flex-col">
           <p className="Strong">The Last Survivor on the space</p>
-          <p className="Sfont">Shoot'em up d'alien, top-down shooter blabla</p>
+          <p className="text-lg break-normal pt-4">
+            Shoot'em up d'alien, top-down shooter blabla
+          </p>
+          <div className="flex justify-end">
+            <button className="Plus">En savoir plus</button>
+          </div>
         </div>
-        <button className="Plus">En savoir plus</button>
+      </div>
+
+      <div className="SubTitle">
+        <h2 id="ExperienceProfesionnel" className="MfontTitle">
+          Experience Profesionnel
+        </h2>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <div className="WrapperBox Myrhmica w-full h-full bg-no-repeat bg-contain bg-left mr-6"></div>
+
+        <div className="w-1/3 flex flex-col">
+          <p className="Strong">PortraiScopie</p>
+          <p className="text-lg break-normal pt-4">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+          </p>
+          <div className="flex justify-end">
+            <button className="Plus">En savoir plus</button>
+          </div>
+        </div>
       </div>
 
       <div id="Contact-me" className="position">
@@ -105,9 +109,11 @@ function App() {
         </div>
       </div>
       <div className="Footer">
-        <div className="Line"></div>
+        <div className="Line w-4/5 bg-black m-1.5"></div>
         <div className="logo">
-          <img src={Git} />
+          <a href="https://github.com/DeFreitasFlavio">
+            <img src={Git} />
+          </a>
         </div>
       </div>
     </div>
